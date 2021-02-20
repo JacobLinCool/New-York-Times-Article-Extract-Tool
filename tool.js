51e964712267a93045c5cc37070923d45456cc52
JacobLinCool <jacoblincool@gmail.com>
2021-02-20 18:12:20 +0800

    update

--- tool.js
+++ tool.js
@@ -1,43 +1,61 @@
-(async function(){
-    if(!location.href.includes("nytimes")) {
+(async function () {
+    if (!location.href.includes("nytimes")) {
         alert("Only New York Times");
-    }
-    else {
-        let article = await fetch(location.href).then(r=>r.text())
-        let parser = new DOMParser()
-        let doc = parser.parseFromString(article, "text/html")
-        
+    } else {
+        let article = await fetch(location.href).then((r) => r.text());
+        let parser = new DOMParser();
+        let doc = parser.parseFromString(article, "text/html");
+
         let win = window.open();
         win.document.open();
-        // Get Title
-        try{
-            win.document.title = doc.getElementsByClassName("e1h9rw200")[0].innerText;
-            win.document.write("<h2>"+doc.getElementsByClassName("e1h9rw200")[0].innerText+"</h2>");
-        }catch(e){}
-        // Get Subtitle
-        try{
-            win.document.write("<span style='color: gray'>Article "+doc.getElementsByClassName("e16638kd1")[0].innerText+"<br>");
-        } catch(e){}
-        // Get URL
-        try{
-            win.document.write("From <a href='"+location.href+"' target='_blank'>"+location.href+"</a><br>");
-        } catch(e){}
-        // Get Writer 
-        try{
-            win.document.write("By <a href='"+doc.getElementsByClassName("e1jsehar0")[0].href+"' target='_blank'>"+doc.getElementsByClassName("e1jsehar0")[0].innerText+"</a></span>");
-        } catch(e){}
-
-        try{
-            win.document.write("<h2>"+doc.getElementsByClassName("balancedHeadline")[0].innerText+"</h2>");
-        } catch(e){}
-        try{
-            win.document.write("<h3>"+doc.getElementById("article-summary").innerText+"</h3>");
-        } catch(e){}
-        for(var i = 0; i < doc.getElementsByClassName("evys1bk0").length; i++) {
-            win.document.write("<p>"+doc.getElementsByClassName("evys1bk0")[i].innerText + "</p>");
-        }
-        win.document.write("<style>h1,h2,h3,p{font-family: sans-serif;} h3{color: gray}</stlye>");
+        // Write Title
+        try {
+            win.document.write(
+                "<h2>" + doc.querySelector(".e1h9rw200").innerText + "</h2>"
+            );
+            win.document.title = doc.querySelector(".e1h9rw200").innerText;
+        } catch (e) {}
+        // Write URL
+        try {
+            let url = location.origin + location.pathname;
+            win.document.write(
+                `From <a href="${url}" target="_blank">${url}</a><br>`
+            );
+        } catch (e) {}
+        // Write Writer
+        try {
+            win.document.write(
+                `By <a href="${
+                    doc.querySelector(".e1jsehar0").href
+                }" target='_blank'>${
+                    doc.querySelector(".e1jsehar0").innerText
+                }</a></span>`
+            );
+        } catch (e) {}
+        //
+        try {
+            win.document.write(
+                `<h2>${doc.querySelector(".balancedHeadline").innerText}</h2>`
+            );
+        } catch (e) {}
+        // Write Summary
+        try {
+            win.document.write(
+                `<h3 class="summary">${
+                    doc.querySelector("#article-summary").innerText
+                }</h3>`
+            );
+        } catch (e) {}
+        // Write Article
+        try {
+            Array.from(doc.querySelectorAll(".evys1bk0")).forEach((elm) => {
+                win.document.write(`<p>${elm.innerText}</p>`);
+            });
+        } catch (e) {}
+        // Write Styles
+        win.document.write(
+            "<style>*{font-family: sans-serif;} .summary{color: gray}</stlye>"
+        );
         win.document.close();
-        window.win = win;
     }
-})()
+})();
